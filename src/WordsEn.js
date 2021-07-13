b2e7dc44ba6cc209fce50ae1b31930055522595b
banao <tatitachi870@gmail.com>
2021-07-13 21:26:25 +0900

    react-native-swipe-listを仮導入

--- src/WordsEn.js
+++ src/WordsEn.js
@@ -1,69 +1,157 @@
-import React, { useState } from 'react';
-import { Text, View, StyleSheet, FlatList, Keyboard } from 'react-native';
-import { Input, Button, Header, ThemeProvider, Card } from 'react-native-elements';
-import Spinner from 'react-native-loading-spinner-overlay';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState, useRef } from 'react';
+import {
+    Animated,
+    Dimensions,
+    StyleSheet,
+    Text,
+    TouchableHighlight,
+    View,
+} from 'react-native';
+import { SwipeListView } from 'react-native-swipe-list-view';
+import { Header, ThemeProvider } from 'react-native-elements'
+
+const theme = {
+    colors: {
+        primary: 'limegreen'
+    }
+}
+
+const rowTranslateAnimatedValues = {};
+Array(20)
+    .fill('')
+    .forEach((_, i) => {
+        rowTranslateAnimatedValues[`${i}`] = new Animated.Value(1);
+    });
 
 export const WordsEn = () => {
-
-    const theme = {
-        colors: {
-            primary: 'limegreen'
-        }
-    }
-
-    const wordsList = [
-        {
+    const [listData, setListData] = useState(
+        [{
             key: 0,
-            word: 'apple'
-        },
-        {
-            key: 1,
-            word: 'banana'
+            text: 'apple'
         },
         {
             key: 2,
-            word: 'grape'
-        }
-    ]
+            text: 'banana'
+        },
+        {
+            key: 3,
+            text: 'anko'
+        }]
+    );
 
-    const renderItem = ({item}) => {
-        return (
-            <View style={styles.flatlistContent}>
-                <Text style={{fontSize: 30}}>{item.word}</Text>
+    console.log(listData)
+
+    const animationIsRunning = useRef(false);
+
+    const onSwipeValueChange = swipeData => {
+        const { key, value } = swipeData;
+        if (
+            value < -Dimensions.get('window').width &&
+            !animationIsRunning.current
+        ) {
+            animationIsRunning.current = true;
+            Animated.timing(rowTranslateAnimatedValues[key], {
+                toValue: 0,
+                duration: 200,
+                useNativeDriver: false,
+            }).start(() => {
+                const newData = [...listData];
+                const prevIndex = listData.findIndex(item => item.key === key);
+                newData.splice(prevIndex, 1);
+                setListData(newData);
+                animationIsRunning.current = false;
+            });
+        }
+    };
+
+    const renderItem = data => (
+        <Animated.View
+            style={[
+                styles.rowFrontContainer,
+                {
+                    height: rowTranslateAnimatedValues[
+                        data.item.key
+                    ].interpolate({
+                        inputRange: [0, 1],
+                        outputRange: [0, 50],
+                    }),
+                },
+            ]}
+        >
+            <TouchableHighlight
+                onPress={() => console.log('You touched me')}
+                style={styles.rowFront}
+                underlayColor={'#AAA'}
+            >
+                <View>
+                    <Text>I am {data.item.text} in a SwipeListView</Text>
+                </View>
+            </TouchableHighlight>
+        </Animated.View>
+    );
+
+    const renderHiddenItem = () => (
+        <View style={styles.rowBack}>
+            <View style={[styles.backRightBtn, styles.backRightBtnRight]}>
+                <Text style={styles.backTextWhite}>Delete</Text>
             </View>
-        )
-    }
+        </View>
+    );
 
     return (
-        <View>
+        <View style={styles.container}>
             <ThemeProvider theme={theme}>
                 <Header 
-                    centerComponent={{text: '単語一覧', style: {color: 'white', fontSize: 20, height: 25}}}
+                    centerComponent={{text: '単語帳', style: {color: 'white', fontSize: 20, height: 25}}}
+                />
+                <SwipeListView
+                    disableRightSwipe
+                    data={listData}
+                    renderItem={renderItem}
+                    renderHiddenItem={renderHiddenItem}
+                    rightOpenValue={-Dimensions.get('window').width}
+                    onSwipeValueChange={onSwipeValueChange}
+                    useNativeDriver={false}
                 />
             </ThemeProvider>
-            <FlatList 
-                data={wordsList}
-                renderItem={renderItem}
-                keyExtractor={wordsList => `${wordsList.key}`}
-                style={styles.flatlist}
-            />
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
-    flatlist: {
-        borderTopColor: 'lightgray',
-        borderTopWidth: 1,
-        marginBottom: 190
+    container: {
+        backgroundColor: 'white',
+        flex: 1,
     },
-    flatlistContent: {
-        padding: 10,
-        height: 70,
+    backTextWhite: {
+        color: '#FFF',
+    },
+    rowFront: {
+        alignItems: 'center',
+        backgroundColor: '#FFF',
         borderBottomColor: 'lightgray',
         borderBottomWidth: 1,
+        justifyContent: 'center',
+        height: 50,
+    },
+    rowBack: {
         alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+        backgroundColor: 'red',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingLeft: 15,
+    },
+    backRightBtn: {
+        alignItems: 'center',
+        bottom: 0,
+        justifyContent: 'center',
+        position: 'absolute',
+        top: 0,
+        width: 75,
+    },
+    backRightBtnRight: {
+        backgroundColor: 'red',
+        right: 0,
+    },
+});
